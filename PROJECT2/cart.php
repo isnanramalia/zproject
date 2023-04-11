1625b3a2e6bf5d8cc71e7613e77494982e7f0cd1
isnanramalia <isnanuramalia13@gmail.com>
2023-04-12 05:57:40 +0700

    create & call head.php

--- PROJECT2/cart.php
+++ PROJECT2/cart.php
@@ -26,18 +26,8 @@ if (isset($_POST['remove'])) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cart</title>
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
-
-    <!-- Bootstrap CDN -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="./css/style.css">
+    <?php require_once('php/head.php'); ?><!-- fungsi meta dan link source -->
+    <title>cart - skinker</title>
 </head>
 
 <body class="bg-light" style="margin-top: 50px; padding-top: 50px;">
