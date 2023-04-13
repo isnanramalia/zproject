bcfed63a2f10b6fb3c7e0b28715c12068e6db47e
isnanramalia <isnanuramalia13@gmail.com>
2023-04-13 20:34:36 +0700

    script nmh item ke cart

--- PROJECT3/shop.php
+++ PROJECT3/shop.php
@@ -4,87 +4,6 @@
 <head>
     <?php require_once('php/head.php'); ?><!-- fungsi meta dan link source -->
     <title>shop - skinker</title>
-
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $(document).on("click", "#addItem", function(e) {
-                e.preventDefault();
-                var form = $(this).closest(".form-submit");
-                var id = form.find(".pid").val();
-                var name = form.find(".pname").val();
-                var price = form.find(".pprice").val();
-                var image = form.find(".pimage").val();
-                var code = form.find(".pcode").val();
-
-                $.ajax({
-                    url: "action.php",
-                    method: "post",
-                    data: {
-                        pid: id,
-                        pname: name,
-                        pprice: price,
-                        pimage: image,
-                        pcode: code
-                    },
-                    success: function(response) {
-                        $(".alert-message").html(response);
-                        window.scrollTo(0, 0);
-                        load_cart_item_number();
-                    }
-                });
-            });
-        });
-    </script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $(document).on("click", "#addItem", function(e) {
-                e.preventDefault();
-                var form = $(this).closest(".form-submit");
-                var id = form.find(".pid").val();
-                var name = form.find(".pname").val();
-                var price = form.find(".pprice").val();
-                var image = form.find(".pimage").val();
-                var code = form.find(".pcode").val();
-
-                $.ajax({
-                    url: "action.php",
-                    method: "post",
-                    data: {
-                        pid: id,
-                        pname: name,
-                        pprice: price,
-                        pimage: image,
-                        pcode: code
-                    },
-                    success: function(response) {
-                        $(".alert-message").html(response);
-                        window.scrollTo(0, 0);
-                        load_cart_item_number();
-                    }
-                });
-            });
-
-            load_cart_item_number();
-
-            function load_cart_item_number() {
-                $.ajax({
-                    url: "action.php",
-                    method: "get",
-                    data: {
-                        cartItem: "cart_item"
-                    },
-                    success: function(response) {
-                        $("#cart-item").html(response);
-                    }
-
-                });
-            }
-        });
-    </script>
-
-
-
-
 </head>
 
 
@@ -254,6 +173,54 @@
     <footer class="text-center text-lg-start border border-white mt-xl-5 pt-4" style="background-color: white;">
         <?php require_once('php/footer.php') ?>
     </footer>
+
+    <!-- nambah item ke keranjang scr dinamis -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $(document).on("click", "#addItem", function(e) {
+                e.preventDefault();
+                var form = $(this).closest(".form-submit");
+                var id = form.find(".pid").val();
+                var name = form.find(".pname").val();
+                var price = form.find(".pprice").val();
+                var image = form.find(".pimage").val();
+                var code = form.find(".pcode").val();
+
+                $.ajax({
+                    url: "action.php",
+                    method: "post",
+                    data: {
+                        pid: id,
+                        pname: name,
+                        pprice: price,
+                        pimage: image,
+                        pcode: code
+                    },
+                    success: function(response) {
+                        $(".alert-message").html(response);
+                        window.scrollTo(0, 0);
+                        load_cart_item_number();
+                    }
+                });
+            });
+
+            load_cart_item_number();
+
+            function load_cart_item_number() {
+                $.ajax({
+                    url: "action.php",
+                    method: "get",
+                    data: {
+                        cartItem: "cart_item"
+                    },
+                    success: function(response) {
+                        $("#cart-item").html(response);
+                    }
+
+                });
+            }
+        });
+    </script>
 </body>
 
 </html>
