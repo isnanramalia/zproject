08271a3cf6f22558c0c5389faff0f63432ad5cdd
isnanramalia <isnanuramalia13@gmail.com>
2023-04-13 20:29:56 +0700

    + file dr onlyxcodes.com

--- PROJECT3/php/head.php
+++ PROJECT3/php/head.php
@@ -23,6 +23,11 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h355rYXK/7HAuoJl+0I4" crossorigin="anonymous">
 </script>
 
+<!-- Bootstrap core JavaScript (dr onlyxcodes.com)-->
+<script src="jquery/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
 <!-- link icon -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
